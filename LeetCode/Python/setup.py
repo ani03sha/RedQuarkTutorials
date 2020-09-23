46ad68e37a6f7f07f17fc91248d9051cfe6bd7c0
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-09-23 10:49:34 +0530

    Added Python project structure for LeetCode

---
+++ LeetCode/Python/setup.py
@@ -0,0 +1,9 @@
+project_info = ["name - LeetCode",
+                "version = 1.0",
+                "description = LeetCode problems and their solutions in python",
+                "author = Anirudh Sharma",
+                "author_email = anirudh03sharma@gmail.com",
+                "repo = https://github.com/ani03sha/RedQuarkTutorials/LeetCode/Python",
+                ]
+
+print(project_info)

ade5408d627ee0c41e68b5cecc83e224403b2398
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-09-30 23:03:00 +0530

    LeetCode 0002 - Longest Substring Without Repeating Characters (Kotlin)

---
+++ LeetCode/Kotlin/src/main/kotlin/org/redquark/tutorials/leetcode/LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,45 @@
+package org.redquark.tutorials.leetcode
+
+import java.util.*
+
+private fun lengthOfLongestSubstring(s: String): Int {
+    // Base condition
+    if (s == "") {
+        return 0
+    }
+    // Starting window index
+    var start = 0
+    // Ending window index
+    var end = 0
+    // Maximum length of substring
+    var maxLength = 0
+    // This set will store the unique characters
+    val uniqueCharacters: MutableSet<Char> = HashSet()
+    while (end < s.length) {
+        if (uniqueCharacters.add(s[end])) {
+            end++
+            maxLength = Math.max(maxLength, uniqueCharacters.size)
+        } else {
+            uniqueCharacters.remove(s[start])
+            start++
+        }
+    }
+    return maxLength
+}
+
+fun main() {
+    var s = "abcabcbb"
+    println(lengthOfLongestSubstring(s))
+
+    s = "bbbbb"
+    println(lengthOfLongestSubstring(s))
+
+    s = "pwwkew"
+    println(lengthOfLongestSubstring(s))
+
+    s = ""
+    println(lengthOfLongestSubstring(s))
+
+    s = "aab"
+    println(lengthOfLongestSubstring(s))
+}

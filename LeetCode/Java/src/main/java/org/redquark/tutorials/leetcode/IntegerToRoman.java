89e5cc259e7ad44d44cebf7611bbbfc33dd45027
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-11-07 19:32:37 +0530

    Added LeetCode 12 - Integer To Roman (Java)

---
+++ LeetCode/Java/src/main/java/org/redquark/tutorials/leetcode/IntegerToRoman.java
@@ -0,0 +1,24 @@
+package org.redquark.tutorials.leetcode;
+
+/**
+ * @author Anirudh Sharma
+ */
+public class IntegerToRoman {
+
+    private static String intToRoman(int num) {
+        String[] M = {"", "M", "MM", "MMM"};
+        String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(3));
+        System.out.println(intToRoman(4));
+        System.out.println(intToRoman(9));
+        System.out.println(intToRoman(58));
+        System.out.println(intToRoman(1994));
+    }
+}

2335c5e1f8e704326663717316037a9cd6e45f10
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-11-08 19:38:53 +0530

    Added LeetCode 14 - Longest Common Prefix (JavaScript)

---
+++ LeetCode/JavaScript/src/longest_common_prefix.js
@@ -0,0 +1,29 @@
+var longestCommonPrefix = function (strs) {
+    // Longest common prefix string
+    let longestCommonPrefix = "";
+    // Base condition
+    if (strs == null || strs.length == 0) {
+        return longestCommonPrefix;
+    }
+    // Find the minimum length string from the array
+    let minimumLength = strs[0].length;
+    for (let i = 1; i < strs.length; i++) {
+        minimumLength = Math.min(minimumLength, strs[i].length);
+    }
+    // Loop for the minimum length
+    for (let i = 0; i < minimumLength; i++) {
+        // Get the current character from first string
+        let current = strs[0][i];
+        // Check if this character is found in all other strings or not
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i] != current) {
+                return longestCommonPrefix;
+            }
+        }
+        longestCommonPrefix += current;
+    }
+    return longestCommonPrefix;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));

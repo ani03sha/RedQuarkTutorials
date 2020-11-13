199d99bf66c7ea22aacc3577b1c2b017718d4c35
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-11-13 21:13:21 +0530

    Added LeetCode 18 - 4 Sum (Java)

--- LeetCode/Java/src/main/java/org/redquark/tutorials/leetcode/FourSum.java
+++ LeetCode/Java/src/main/java/org/redquark/tutorials/leetcode/FourSum.java
@@ -61,9 +61,9 @@ public class FourSum {
     }
 
     public static void main(String[] args) {
-        /*System.out.println(fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
+        System.out.println(fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
         System.out.println(fourSum(new int[]{}, 0));
-        System.out.println(fourSum(new int[]{1, 2, 3, 4}, 10));*/
+        System.out.println(fourSum(new int[]{1, 2, 3, 4}, 10));
         System.out.println(fourSum(new int[]{0, 0, 0, 0}, 0));
     }
 }

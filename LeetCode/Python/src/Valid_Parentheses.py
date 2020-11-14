f59e3430b9e920d7162e369d3ffb9a21bd457dee
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-11-14 21:15:14 +0530

    Added LeetCode 20 - Valid Parentheses (Python)

---
+++ LeetCode/Python/src/Valid_Parentheses.py
@@ -0,0 +1,32 @@
+"""
+@author - Anirudh Sharma
+"""
+
+
+def isValid(s: str) -> bool:
+    # Stack for left symbols
+    leftSymbols = []
+    # Loop for each character of the string
+    for c in s:
+        # If left symbol is encountered
+        if c in ['(', '{', '[']:
+            leftSymbols.append(c)
+        # If right symbol is encountered
+        elif c == ')' and len(leftSymbols) != 0 and leftSymbols[-1] == '(':
+            leftSymbols.pop()
+        elif c == '}' and len(leftSymbols) != 0 and leftSymbols[-1] == '{':
+            leftSymbols.pop()
+        elif c == ']' and len(leftSymbols) != 0 and leftSymbols[-1] == '[':
+            leftSymbols.pop()
+        # If none of the valid symbols is encountered
+        else:
+            return False
+    return leftSymbols == []
+
+
+if __name__ == '__main__':
+    print(isValid("()"))
+    print(isValid("()[]{}"))
+    print(isValid("(]"))
+    print(isValid("([)]"))
+    print(isValid("{[]}"))

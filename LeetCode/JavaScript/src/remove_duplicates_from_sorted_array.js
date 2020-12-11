d001d61e205eac865644be588c2dda86d053e3d9
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-12-11 20:20:36 +0530

    Added LeetCode 26 - Remove Duplicaes From Sorted Array (JavaScript)

---
+++ LeetCode/JavaScript/src/remove_duplicates_from_sorted_array.js
@@ -0,0 +1,18 @@
+var removeDuplicates = function (nums) {
+    // Length of the updated array
+    let count = 0;
+    // Loop for all the elements in the array
+    for (let i = 0; i < nums.length; i++) {
+        // If the current element is equal to the next element, we skip
+        if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+            continue;
+        }
+        // We will update the array in place
+        nums[count] = nums[i];
+        count++;
+    }
+    return count;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));

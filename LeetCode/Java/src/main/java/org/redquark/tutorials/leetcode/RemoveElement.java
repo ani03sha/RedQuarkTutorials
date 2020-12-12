82876060b84ad015693a85a1672446f93180211d
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-12-12 21:46:43 +0530

    Added LeetCode 27 - Remove Element (Java)

---
+++ LeetCode/Java/src/main/java/org/redquark/tutorials/leetcode/RemoveElement.java
@@ -0,0 +1,25 @@
+package org.redquark.tutorials.leetcode;
+
+/**
+ * @author Anirudh Sharma
+ */
+public class RemoveElement {
+
+    private static int removeElement(int[] nums, int val) {
+        // Counter for keeping track of elements other than val
+        int count = 0;
+        // Loop through all the elements of the array
+        for (int i = 0; i < nums.length; i++) {
+            // If the element is not val
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+        System.out.println(removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
+    }
+}

98b5bc46aa3a10c07de5339a059a999860608050
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-12-16 11:31:50 +0530

    Added LeetCode 29 - Divide Two Integers (Java)

--- LeetCode/Java/src/main/java/org/redquark/tutorials/leetcode/DivideTwoIntegers.java
+++ LeetCode/Java/src/main/java/org/redquark/tutorials/leetcode/DivideTwoIntegers.java
@@ -14,18 +14,23 @@ public class DivideTwoIntegers {
         int sign = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) ? -1 : 1;
         // Quotient
         int quotient = 0;
+        // Take the absolute value
         long absoluteDividend = Math.abs((long) dividend);
         long absoluteDivisor = Math.abs((long) divisor);
-        // loop until sum is less than the dividend
+        // Loop until the  dividend is greater than divisor
         while (absoluteDividend >= absoluteDivisor) {
+            // This represents the number of bits shifted or
+            // how many times we can double the number
             int shift = 0;
             while (absoluteDividend >= (absoluteDivisor << shift)) {
                 shift++;
             }
+            // Add the number of times we shifted to the quotient
             quotient += (1 << (shift - 1));
+            // Update the dividend for the next iteration
             absoluteDividend -= absoluteDivisor << (shift - 1);
         }
-        return quotient * sign;
+        return sign == -1 ? -quotient : quotient;
     }
 
     public static void main(String[] args) {
